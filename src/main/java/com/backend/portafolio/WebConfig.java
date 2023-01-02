e1591a6f798c698a8da7ef290cf54bae2dbb2fac
KevinFle <kevin.fle@hotmail.com>
2023-01-02 11:10:04 -0300

    actualizacion

--- src/main/java/com/backend/portafolio/WebConfig.java
+++ src/main/java/com/backend/portafolio/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**");
